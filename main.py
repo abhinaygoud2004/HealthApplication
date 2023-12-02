b78258b96497351865aec39b25dc12764bddcf46
Sai Nandan <agsnandan2004@gmail.com>
2023-12-02 12:19:04 +0530

    nutrition page design

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import pandas as pd
 from sklearn import metrics
 from xgboost import XGBRegressor
 from sklearn.model_selection import train_test_split
-
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import make_pipeline
 
 app = Flask(__name__)
 CORS(app)
@@ -20,7 +23,9 @@ Y = cal_data['Calories']
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=2)
 
 # model
-model = XGBRegressor()
+# model = XGBRegressor()
+degree = 3
+model = make_pipeline(PolynomialFeatures(degree), LinearRegression())
 model.fit(X_train, Y_train)
 
 
